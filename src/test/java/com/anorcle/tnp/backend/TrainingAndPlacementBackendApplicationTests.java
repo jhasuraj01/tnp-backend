9e37e9d3afe6d9471fba67e75c1b7faa393e9a55
jhasuraj01 <44930179+jhasuraj01@users.noreply.github.com>
2023-08-27 18:44:45 +0530

    Project Initialized

---
+++ src/test/java/com/anorcle/tnp/backend/TrainingAndPlacementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anorcle.tnp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingAndPlacementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
